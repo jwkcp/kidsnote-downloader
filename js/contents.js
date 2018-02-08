3fdf2cc3cdd115e4d22d81a750b333310f13cd59
김형조 <hyoungjkim@ebay.com>
2018-02-08 14:05:05 +0900

    사전 작업 제거, Promise 적용, 파일명 앨범 날짜 적용, 원본다운로드 적용.

--- js/contents.js
+++ js/contents.js
@@ -16,15 +16,6 @@ $("document").ready(function() {
     // 각 파일에 붙일 접두사(Prefix)
     const eachFileNamePrefix = "kids_picture_";
 
-    // 수집된 사진을 저장할 배열
-    var arrayDownloadUrl = Array();
-    // Javascript로 일괄 다운로드를 구현하기 위한 JSZip 객체 생성 (https://stuk.github.io/jszip/)
-    var zip = new JSZip();
-    // 다운로드 대상 수
-    var cntTargets = 0;
-    // 다운로드 완료 수
-    var cntDownloads = 0;
-
     /************************************************************/
     /* 페이지가 로딩되면 사용자들이 가장 잘 보이는 위치인 최상단에 버튼 생성
     /************************************************************/
@@ -34,92 +25,146 @@ $("document").ready(function() {
         text: "여기를 누르시면 모든 앨범의 사진과 동영상을 일괄 다운로드합니다."
     }));
 
-    /************************************************************/
-    /* [사전작업] Kidsnote 사이트에서 페이지/앨범/사진 정보 추출
-    /************************************************************/
-    $.get(pageUrl, function(pageData) {
-        // 앨범이 페이징되어 있을 경우 총 페이지 수 추출
-        var pageHtml = $("<pageData>").append($.parseHTML(pageData));
-        var pageResult = $("ul.pagination.pagination-sm", pageHtml);
-        var pageLimit = pageResult.find("li").length - 1;  // 꺽쇠 1개를 삭제
-        var eachAlbumUrl = "";
+    // 앨범 다운로드
+    function run() {
+      // Javascript로 일괄 다운로드를 구현하기 위한 JSZip 객체 생성 (https://stuk.github.io/jszip/)
+      var zip = new JSZip();
 
-        // 추출된 페이지 수만큼 작업을 반복
-        for (var i=1; i<=pageLimit; i++) {
-            // 앨범 링크 추출
-            $.get(albumUrl + i, function(albumData) {
-                var albumHtml = $("<albumData>").append($.parseHTML(albumData));
-                var albumResult = $("div.album-list-wrapper", albumHtml);
+      $("#btnDownload").text("분석 중...");
+      createAlbumPageIndexPromise() // 페이지 index 추출
+      .then(indexes => // 페이지별 앨범 그룹 추출
+        Promise.all(indexes.map(index => createAlbumPromise(index)))
+      )
+      .then(albumGroups => // 앨범별 다운로드 링크 추출
+        Promise.all(albumGroups
+          .reduce((a,b) => a.concat(b))
+          .map(album => createDownloadLinkPromise(album)))
+      )
+      .then(albums => // 다운로드
+        Promise.all(createDownloadPromise(albums, zip))
+      )
+      .then(() => // 압축
+        zip.generateAsync({type:"blob"})
+      )
+      .then(blob => { // 저장
+          saveAs(blob, zipFileName);
+          $("#btnDownload").text("완료!");
+      });
+    }
 
-                albumResult.find("a").each(function () {
-                    // 각 앨범 주소 추출
-                    eachAlbumUrl = baseUrl + $(this).attr("href");
+    // 앨범 페이지 index 추출
+    function createAlbumPageIndexPromise() {
+      return new Promise((resolve, reject) => {
+        $.get(pageUrl, pageData => {
+            // 앨범이 페이징되어 있을 경우 총 페이지 수 추출
+            var pageLimit = $(pageData).find("ul.pagination.pagination-sm li").length - 1; // 꺽쇠 1개를 삭제
 
-                    // 개별 다운로드 파일 링크 추출
-                    $.get(eachAlbumUrl, function(picData) {
-                        var picHtml = $("<picData>").append($.parseHTML(picData));
+            var pageIndexes = [];
+            // 추출된 페이지 수만큼 작업을 반복
+            for (var i=1; i<=pageLimit; i++) pageIndexes.push(i);
 
-                        // 사진 파일 주소 추출
-                        var picResult = $("div.grid", picHtml);
-                        picResult.find("a").each(function () {
-                            arrayDownloadUrl.push($(this).attr("href"));
-                        });
+            resolve(pageIndexes);
+          });
+      });
+    }
 
-                        // 동영상 파일 주소 추출
-                        var movResult = $("div.download-button-wrapper", picHtml);
-                        movResult.find("a").each(function () {
-                            //console.log($(this).attr('href'));
-                            arrayDownloadUrl.push($(this).attr("href"));
-                        });
-                    });
-                });
-            }).fail(function() {
-                console.log("Function failed.");
+    // 앨범 정보 생성 Promise
+    function createAlbumPromise(pageIndex) {
+      return new Promise(function(resolve, reject){
+        // 페이별
+        $.get(albumUrl + pageIndex, albumData => {
+            var albums = $(albumData).find("div.album-list-wrapper a").map(function () {
+                // 앨범정보 추출
+                var dateNumbers = $(this).find('.pull-left.card-footer-text').text().trim().split('.');
+                return album = {
+                  url: baseUrl + $(this).attr("href"),
+                  date: new Date(dateNumbers[0], dateNumbers[1] - 1, dateNumbers[2]),
+                  title: $(this).find('.card-sub-title').text().trim(),
+                  pictures:[],
+                  videos:[]
+                };
+
+            }).toArray();
+            resolve(albums);
+        });
+      });
+    }
+
+    // 개별 다운로드 파일 링크 추출 Promise 생성
+    function createDownloadLinkPromise(album) {
+      return new Promise((resolve, reject) => {
+        // 개별 다운로드 파일 링크 추출
+        $.get(album.url, function(picData) {
+            var picHtml = $(picData);
+
+            // 사진 파일 주소 추출
+            var picResult = picHtml.find("div.grid");
+            picResult.find("a").each(function () {
+                album.pictures.push($(this).attr("data-download"));
             });
-        }
-    });
+
+            // 동영상 파일 주소 추출
+            var movResult = picHtml.find("div.download-button-wrapper");
+            movResult.find("a").each(function () {
+                //console.log($(this).attr('href'));
+                album.videos.push($(this).attr("href"));
+            });
+
+            resolve(album);
+        });
+      });
+    }
+
+    // 개별 다운로드 Promise 배열 생성
+    function createDownloadPromise(albums, zip) {
+      // 다운로드 진행상황
+      var cntDownloads = 0;
+      var totalCount = 0;
+
+      return albums.map(album => {
+        var dateString = formatDate(album.date);
+        totalCount += album.pictures.length;
+        totalCount += album.videos.length;
+
+        // 다운로드
+        return [
+          album.pictures.map((url, index) => {return {url: url, fileName: 'album_' + dateString + '_' + index + '.jpg'};}),
+          album.videos.map((url, index) => {return {url: url, fileName: 'album_' + dateString + '_' + index + '.mp4'};})
+        ]
+        .reduce((a,b) => a.concat(b))
+        .map(item => {
+          return new Promise((resolve, reject) => {
+            // 수집된 각 개별 파일을 다운로드하여 압축하기 위해 JSZip 수행
+            JSZipUtils.getBinaryContent(item.url, (err, data) => {
+                if (err) {
+                  reject(err);
+                  return;
+                }
+                zip.file(item.fileName, data);
+                cntDownloads++; // 실제 다운로드 된 수 (콜백이기 때문에)
+                $("#btnDownload").text("진행률: " + Math.round((cntDownloads * 100) / totalCount) + "%" + "(" + cntDownloads + "/" + totalCount + ")");
+                resolve();
+            });
+          });
+        });
+      }).reduce((a,b) => a.concat(b));
+    }
 
     /************************************************************/
     /* 파일 다운로드 버튼 클릭
     /************************************************************/
-    $("#btnDownload").click(function() {
-        $("#btnDownload").text("분석 중...");
+    $("#btnDownload").click(() => run());
 
-        arrayDownloadUrl.forEach(function(url) {
-            // 사진과 동영상의 파일 구분에 따른 확장자 부여
-            var eachFileName = "kids_picture_default_name";
-            if (url.indexOf(".mp4") > -1) {
-                eachFileName = eachFileNamePrefix + cntTargets + ".mp4";
-            }
-            else {
-                eachFileName = eachFileNamePrefix + cntTargets + ".jpg";
-            }
+    // 날짜 yyy-mm-dd 반환
+    function formatDate(date) {
+        var d = date,
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
 
-            // 수집된 각 개별 파일을 다운로드하여 압축하기 위해 JSZip 수행
-            JSZipUtils.getBinaryContent(url, function (err, data) {
-                if(err) {
-                    throw err;
-                }
-                zip.file(eachFileName, data);
-                cntDownloads++; // 실제 다운로드 된 수 (콜백이기 때문에)
-            });
-            cntTargets++; // 개별 파일 네이밍을 위해 카운팅된 수량
-        });
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
 
-        // 2초 마다 1번씩 콜백에 따른 다운로드 진행률을 체크하여 모두 완료 시 파일을 Zip으로 사용자 컴퓨터에 저장
-        var monitor = setInterval(function() {
-            $("#btnDownload").text("진행률: " + Math.round((cntDownloads * 100) / arrayDownloadUrl.length) + "%" + "(" + cntDownloads + "/" + arrayDownloadUrl.length + ")");
-
-            if (cntDownloads >= arrayDownloadUrl.length) {
-                clearInterval(monitor);
-
-                zip.generateAsync({type:"blob"})
-                    .then(function(blob)
-                    {
-                        saveAs(blob, zipFileName);
-                        $("#btnDownload").text("완료!");
-                    });
-            }
-        }, 2000);
-    });
+        return [year, month, day].join('-');
+    }
 });
